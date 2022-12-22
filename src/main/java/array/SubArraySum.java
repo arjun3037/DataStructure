42869b317261b1851ff4c582c344a6bbc9432e57
arjun3037 <arjun3037@gmail.com>
2022-12-22 12:07:01 +0530

    SubArray Sum solution

---
+++ src/main/java/array/SubArraySum.java
@@ -0,0 +1,44 @@
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    
+    {
+        ArrayList<Integer> array = new ArrayList<>();
+        
+        if (s == 0){
+            array.add(-1);
+            return array;
+        }
+        // Your code here
+        
+        int currSum = arr[0];
+        int j =0;
+        for(int i = 1 ; i <= n ; i++){
+            
+            while(currSum > s && j < n-1){
+                currSum =currSum - arr[j];
+                j++;
+            }
+            
+            if(currSum == s){
+                array.add(j+1);
+                array.add(i);
+                break;
+            }
+            
+            if(i < n){
+                currSum =currSum + arr[i];    
+            }    
+            
+            
+        }
+        
+        if(array.isEmpty()){
+            array.add(-1);
+        }
+        return array;
+    }
+}
